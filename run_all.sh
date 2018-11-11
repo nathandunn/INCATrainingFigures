90a370c0a822c233e5fcae149bd96bf83c993813
Nathan Dunn <ndunn@me.com>
2018-11-11 18:49:35 +0000

    small resutls repo

---
+++ run_all.sh
@@ -0,0 +1,18 @@
+#/bin/sh 
+
+#../figtools analyze --json `cat 2.txt` > output2.json
+../figtools analyze --json `cat 3.txt` > output3.json
+../figtools analyze --json `cat 4.txt` > output4.json
+../figtools analyze --json `cat 5.txt` > output5.json
+../figtools analyze --json `cat 6.txt` > output6.json
+../figtools analyze --json `cat 7.txt` > output7.json
+../figtools analyze --json `cat 8.txt` > output8.json
+../figtools analyze --json `cat 9.txt` > output9.json
+../figtools analyze --json `cat 10.txt` > output10.json
+../figtools analyze --json `cat 11.txt` > output11.json
+../figtools analyze --json `cat 12.txt` > output12.json
+../figtools analyze --json `cat 13.txt` > output13.json
+../figtools analyze --json `cat 14.txt` > output14.json
+../figtools analyze --json `cat 15.txt` > output15.json
+../figtools analyze --json `cat 16.txt` > output16.json
+../figtools analyze --json `cat 17.txt` > output17.json
